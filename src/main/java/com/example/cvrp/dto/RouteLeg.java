c54f1d10b0d073842f3b517362837de068211328
Önder <ondertzn@hotmail.com>
2023-12-29 01:14:00 +0300

    @Data is added to get rid of getters and setters.

--- src/main/java/com/example/cvrp/dto/RouteLeg.java
+++ src/main/java/com/example/cvrp/dto/RouteLeg.java
@@ -1,40 +1,20 @@
 package com.example.cvrp.dto;
 
+import lombok.Data;
+
+@Data
 public class RouteLeg {
     private Long destinationId;
+    private Double latitude;
+    private Double longitude;
     private Double time;
     private Double distance;
 
-    public RouteLeg(Long destinationId, Double time, Double distance) {
+    public RouteLeg(Long destinationId, Double latitude, Double longitude, Double time, Double distance) {
         this.destinationId = destinationId;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.time = time;
-        this.distance = distance;
-    }
-
-    // Getter and setter for destinationId
-    public Long getDestinationId() {
-        return destinationId;
-    }
-
-    public void setDestinationId(Long destinationId) {
-        this.destinationId = destinationId;
-    }
-
-    // Getter and setter for time
-    public Double getTime() {
-        return time;
-    }
-
-    public void setTime(Double time) {
-        this.time = time;
-    }
-
-    // Getter and setter for distance
-    public Double getDistance() {
-        return distance;
-    }
-
-    public void setDistance(Double distance) {
         this.distance = distance;
     }
 }

961c86c9faab9f4dfa7ada88f55b32ff99690564
Önder <ondertzn@hotmail.com>
2023-12-30 01:19:26 +0300

    The vehicle capacity restriction is added. Capacity is considered, too.

--- src/main/java/com/example/cvrp/service/DistanceServiceImp.java
+++ src/main/java/com/example/cvrp/service/DistanceServiceImp.java
@@ -19,40 +19,53 @@ public class DistanceServiceImp {
         this.addressService = addressService;
     }
 
-    public List<RouteLeg> calculateOptimalRoute(int addressLimit, int vehicleCapacity) {
+    public List<RouteLeg> calculateOptimalRoute(int addressLimit, Long vehicleCapacity) {
+        // Increment addressLimit by 1 to account for the depot
+        addressLimit = addressLimit + 1;
+
         List<Address> allAddresses = new ArrayList<>(addressService.findAllAddresses(addressLimit));
         List<RouteLeg> route = new ArrayList<>();
+        Address depot = allAddresses.get(0);   // Store the starting point
 
-        Address depot = allAddresses.get(0);
-        int currentCapacity = vehicleCapacity;
+        // Add the depot as the starting point in the route
+        route.add(new RouteLeg(depot.getId(), depot.getLatitude(), depot.getLongitude()));
+
+        Long currentCapacity = vehicleCapacity;
         Address origin = depot;
+
+        // Remove depot from allAddresses to avoid considering it as a next stop
+        allAddresses.remove(depot);
 
         while (!allAddresses.isEmpty()) {
             Address nextAddress = findFeasibleDestinationWithCapacity(origin, allAddresses, currentCapacity);
 
             if (nextAddress == null) {
-                route.add(createRouteLegToDepot(origin, depot));
-                currentCapacity = vehicleCapacity;
+                // Return to depot if no feasible destination is found
+                RouteLeg legToDepot = createRouteLegToDepot(origin, depot);
+                route.add(legToDepot);
                 origin = depot;
+                currentCapacity = vehicleCapacity;
+                continue;
             }
-            else {
-                RouteLeg leg = createRouteLeg(origin, nextAddress);
-                route.add(leg);
-                currentCapacity -= nextAddress.getUnit();
-                allAddresses.remove(nextAddress);
-                origin = nextAddress;
-            }
+
+            // Visit the next address
+            RouteLeg leg = createRouteLeg(origin, nextAddress);
+            route.add(leg);
+            currentCapacity -= nextAddress.getUnit();
+            allAddresses.remove(nextAddress);
+            origin = nextAddress;
+        }
+
+        // Return to depot at the end of the route
+        if (!origin.equals(depot)) {
+            route.add(createRouteLegToDepot(origin, depot));
         }
 
         return route;
     }
 
-    private RouteLeg createRouteLegToDepot(Address origin, Address depot) {
-        return getRouteLeg(origin, depot);
-    }
 
-
-    private Address findFeasibleDestinationWithCapacity(Address origin, List<Address> potentialDestinations, int currentCapacity) {
+    private Address findFeasibleDestinationWithCapacity(Address origin, List<Address> potentialDestinations, Long currentCapacity) {
         Address optimalDestination = null;
         Double shortestTime = Double.MAX_VALUE;
         Double shortestDistance = Double.MAX_VALUE;
@@ -74,27 +87,80 @@ public class DistanceServiceImp {
         return optimalDestination;
     }
 
-    private RouteLeg createRouteLeg(Address origin, Address destination) {
-        return getRouteLeg(origin, destination);
+    private GoogleMapsResponse getGoogleMapsResponse(Address origin, Address destination) {
+        try {
+            // Prepare the parameters for the Google Maps API request
+            String originParam = origin.getLatitude() + "," + origin.getLongitude();
+            String destinationParam = destination.getLatitude() + "," + destination.getLongitude();
+
+            // Call Google Maps API and return the response
+            return googleMapsService.getDistanceMatrix(originParam, destinationParam);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    private RouteLeg getRouteLeg(Address origin, Address destination) {
+    private RouteLeg extractTimeAndDistance(Address origin, Address destination) {
         GoogleMapsResponse response = getGoogleMapsResponse(origin, destination);
+        if (response == null || response.getRows() == null || response.getRows().isEmpty()) {
+            // Handle the error scenario, possibly by logging and returning null or a default RouteLeg
+            System.out.println("Error: Unable to get response from Google Maps for " + origin + " to " + destination);
+            return null;
+        }
+
         Double time = response.getRows().get(0).getElements().get(0).getDuration().getValue();
         Double distance = response.getRows().get(0).getElements().get(0).getDistance().getValue();
 
-        return new RouteLeg(destination.getId(),
-                destination.getLatitude(),
-                destination.getLongitude(),
-                time,
-                distance);
+        return new RouteLeg(destination.getId(), destination.getLatitude(), destination.getLongitude(), time, distance);
     }
 
-    private GoogleMapsResponse getGoogleMapsResponse(Address origin, Address destination) {
-        String originParam = origin.getLatitude() + "," + origin.getLongitude();
-        String destinationParam = destination.getLatitude() + "," + destination.getLongitude();
 
-        return googleMapsService.getDistanceMatrix(originParam, destinationParam);
+    private RouteLeg createRouteLegToDepot(Address origin, Address depot) {
+        return extractTimeAndDistance(origin, depot);
+    }
+
+    private RouteLeg createRouteLeg(Address origin, Address nextAddress) {
+        return extractTimeAndDistance(origin, nextAddress);
+    }
+
+    private RouteLeg findOptimalDestination(Address origin, List<Address> potentialDestinations) {
+        RouteLeg optimalLeg = null;
+        Double shortestTime = Double.MAX_VALUE;
+        Double shortestDistance = Double.MAX_VALUE;
+
+        for (Address destination : potentialDestinations) {
+            // Skip if destination is the same as origin
+            if (destination.getId().equals(origin.getId())) {
+                continue;
+            }
+
+            // Prepare the parameters for the Google Maps API request
+            String originParam = origin.getLatitude() + "," + origin.getLongitude();
+            String destinationParam = destination.getLatitude() + "," + destination.getLongitude();
+
+            // Call Google Maps API via GoogleMapsService
+            GoogleMapsResponse response = googleMapsService.getDistanceMatrix(originParam, destinationParam);
+
+            // Extract time and distance from the response
+            // Assume response structure matches your GoogleMapsResponse class
+            Double time = response.getRows().get(0).getElements().get(0).getDuration().getValue();
+            Double distance = response.getRows().get(0).getElements().get(0).getDistance().getValue();
+
+            // Determine if this destination is the new optimal destination
+            if (time < shortestTime || (time.equals(shortestTime) && distance < shortestDistance)) {
+                shortestTime = time;
+                shortestDistance = distance;
+                optimalLeg = new RouteLeg(
+                        destination.getId(),
+                        destination.getLatitude(),
+                        destination.getLongitude(),
+                        time,
+                        distance);
+            }
+        }
+
+        return optimalLeg;
     }
 
     private Address getAddressById(Long id, List<Address> addresses) {

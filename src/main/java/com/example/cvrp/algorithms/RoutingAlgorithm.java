9ef04a405bfa6f24bc050a254c689be5c31530f0
Önder <ondertzn@hotmail.com>
2024-05-08 23:11:37 +0300

    calculateRouteWithDepot is added

--- src/main/java/com/example/cvrp/algorithms/RoutingAlgorithm.java
+++ src/main/java/com/example/cvrp/algorithms/RoutingAlgorithm.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public interface RoutingAlgorithm {
 
+    List<RouteLeg> calculateRouteWithDepot(Address depot, List<Address> addresses, long vehicleCapacity);
+
     List<RouteLeg> calculateRoute(List<Address> addresses, Long vehicleCapacity);
+
+
+    //List<RouteLeg> calculateRoute(Address depot, List<Address> addresses, long vehicleCapacity);
 
 }
